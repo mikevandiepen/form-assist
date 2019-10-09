6317a41bfcff3cf12fb2ff2f0102ec17ee972f62
Mike van Diepen <mike.vandiepen@hotmail.com>
2019-10-09 17:24:00 +0200

    Conformed classes to interface

--- src/Validate/StringValidator.php
+++ src/Validate/StringValidator.php
@@ -5,10 +5,24 @@ namespace mikevandiepen\utility\Validate;
 class StringValidator implements ValidationInterface
 {
     /**
+     * Validation constructor.
+     *
+     * @param string $attribute
+     * @param string $value
+     */
+    public function __construct(string $attribute, string $value)
+    {
+
+    }
+
+    /**
      * Validating the assigned rule and returning output
+     *
+     * @param array $parameters
+     *
      * @return string
      */
-    public function validate() : string
+    public function validate(array $parameters = array()) : string
     {
         // TODO: Implement validate() method.
     }
